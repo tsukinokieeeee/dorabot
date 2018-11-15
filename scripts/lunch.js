8f22ffc8778966354f3512118b5456d74cd4325f
大槻晃也 <>
2018-11-15 13:09:12 +0900

    no message

--- scripts/lunch.js
+++ scripts/lunch.js
@@ -1,14 +1,14 @@
 // Description:
 //   飯決めてくれるbot
 
-// module.exports = function(robot) {
+module.exports = function(robot) {
 
-//       var foodarray = ["チャーハン","ラーメン","サンドイッチ","マクド","定食","魚","カレー","天丼","焼肉","うどん","そば","パスタ","コンビニ弁当"];
-//       var gobi = ["食え","、どう？","とかどうすか","でも食っとけ","はいかがでしょうか。","の気分じゃない?","食べたら?"];
+      var foodarray = ["チャーハン","ラーメン","サンドイッチ","マクド","定食","魚","カレー","天丼","焼肉","うどん","そば","パスタ","コンビニ弁当"];
+      var gobi = ["食え","、どう？","とかどうすか","でも食っとけ","はいかがでしょうか。","の気分じゃない?","食べたら?"];
 
-//       robot.respond(/昼飯|ランチ|昼ご飯|昼ごはん|昼御飯/i, function(msg) {
+      robot.respond(/昼飯|ランチ|昼ご飯|昼ごはん|昼御飯/i, function(msg) {
 
-//             var response = msg.random(foodarray) + msg.random(gobi);
-//             msg.send(response);
-//       });
-// };
+            var response = msg.random(foodarray) + msg.random(gobi);
+            msg.send(response);
+      });
+};
