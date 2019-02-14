dc61ac2893fe82b4d60030569b10d7826dee6102
morita <morita@interlifemedia.co.jp>
2019-02-14 18:58:03 +0900

    わからんけどとまった

--- scripts/fgo.js
+++ scripts/fgo.js
@@ -30,7 +30,7 @@ var gobi = ["「戦闘です。マスター、指示を」",
             msg.send({
       as_user: false,
       text: response,
-      username: マシュ,
+      username: "マシュ",
       icon_url: "https://www.google.co.jp/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwijgI3S-LrgAhUIabwKHZONCPMQjRx6BAgBEAU&url=http%3A%2F%2Fstartcos.com%2Fcharacter%2Ffgo-matthew%2F&psig=AOvVaw3lWd-H8p1REfnRjauL2DSq&ust=1550224016055973",
             });
       });
